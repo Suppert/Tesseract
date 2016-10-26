14ea84f700f361f2065a29a138aa474bb90b64d7
Jake <ImagicalGamer@users.noreply.github.com>
2016-10-26 19:51:47 -0400

    Create config.py

---
+++ tesseract/resources/config.py
@@ -0,0 +1,30 @@
+"""
+THIS IS COPIED FROM THE SPIFFY SERVER SOFTWARRE
+LINK: UNKNOWN
+"""
+
+import json
+import os.path
+
+def generate_config():
+	target = open('settings.json', 'w')
+	config = """
+	{
+		\"port\": 19132,
+		\"name\": \"A Minecraft: PE Server\"
+	}
+	"""
+	parsed = json.loads(config)
+	target.write(json.dumps(parsed, indent = 4, sort_keys = True))
+
+def load_config():
+	with open('settings.json', 'r') as handle:
+		parsed = json.load(handle)
+		return parsed
+
+def handle_config():
+	if os.path.isfile('settings.json'):
+		return load_config()
+	else:
+		generate_config()
+return load_config()
