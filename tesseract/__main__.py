bb34c41defc3e6bcbd96833457911dad8edb6261
Jake <ImagicalGamer@users.noreply.github.com>
2016-10-26 20:05:40 -0400

    Update and rename Tesseract.py to __main__.py

---
+++ tesseract/__main__.py
@@ -0,0 +1,31 @@
+"""
+   _____                                 _   
+  |_   _|                               | |  
+    | | ___  ___ ___  ___ _ __ __ _  ___| |_ 
+    | |/ _ \/ __/ __|/ _ \ '__/ _` |/ __| __|
+    | |  __/\__ \__ \  __/ | | (_| | (__| |_ 
+    \_/\___||___/___/\___|_|  \__,_|\___|\__|                                                                                   
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+"""
+from resources import config
+import sys
+
+try:
+  from pyraklib.server import PyRakLibServer
+  from pyraklib.server import ServerHandler
+  found = true
+except ImportError:
+  found = false
+  pass
+
+if found == false:
+  sys.exit("Unable to load all modules")
+
+config = config.handle_config()
+server = PyRakLibServer(config['port'])
+print("Tesseract is licensed under the GNU license")
+print("starting server on *:" + str(config['port']))
