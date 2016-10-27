cc80e72ff8267dcfc6fa4ac8bcf8efc5c3ebf1a9
Jake <ImagicalGamer@users.noreply.github.com>
2016-10-26 21:23:15 -0400

    Create player.py

---
+++ tesseract/player.py
@@ -0,0 +1,23 @@
+# python.py
+"""
+   _____                                 _   
+  |_   _|                               | |  
+    | | ___  ___ ___  ___ _ __ __ _  ___| |_ 
+    | |/ _ \/ __/ __|/ _ \ '__/ _` |/ __| __|
+    | |  __/\__ \__ \  __/ | | (_| | (__| |_ 
+    \_/\___||___/___/\___|_|  \__,_|\___|\__|                                                                                   
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+"""
+Class Player:
+    GAMEMODE_SURVIVAL = 0
+    GAMEMODE_CREATIVE = 1
+    GAMEMODE_ADVENTURE = 2
+    GAMEMODE_SPECTATOR = 3
+    
+    SURVIVAL_SLOTS = 36
+    CREATIVE_SLOTS = 112
+    
