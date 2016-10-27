b4d687c549a12999103353555f641f21309e8a77
Jake <ImagicalGamer@users.noreply.github.com>
2016-10-26 21:06:15 -0400

    Would that work?

--- tesseract/utils/textformat.py
+++ tesseract/utils/textformat.py
@@ -19,4 +19,5 @@ Class TextFormat():
             text.replace("§n", TextFormat.UNDERLINE)
             text.replace("§d", TextFormat.PURPLE)
             text.replace("§5", TextFormat.PURPLE)
+            return text
 
