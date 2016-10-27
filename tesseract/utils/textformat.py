7710aee8438d555c54df6a56d9cf7cf77b6dad87
Jake <ImagicalGamer@users.noreply.github.com>
2016-10-26 20:53:35 -0400

    Update textformat.py

--- tesseract/utils/textformat.py
+++ tesseract/utils/textformat.py
@@ -1,7 +1,13 @@
 # textformat.py
 Class TextFormat():
-  GREEN = "\x1b[1;32;40m"
-  RED = "\x1b[1;31;40m"
+  
+  GREEN = '\x1b[1;32;40m'
+  RED = '\x1b[1;31;40m'
+  AQUA = '\x1b[1;34;40m'
+  PURPLE = '\x1b[1;35;40m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+  END = '\033[0m'
   
   @staticmethod
   def toANSI(string):
