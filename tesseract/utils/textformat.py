e1a92a11ef54c8375488480b631481e5b5b9861f
Jake <ImagicalGamer@users.noreply.github.com>
2016-10-26 21:23:27 -0400

    Update textformat.py

--- tesseract/utils/textformat.py
+++ tesseract/utils/textformat.py
@@ -1,4 +1,17 @@
 # textformat.py
+"""
+   _____                                 _   
+  |_   _|                               | |  
+    | | ___  ___ ___  ___ _ __ __ _  ___| |_ 
+    | |/ _ \/ __/ __|/ _ \ '__/ _` |/ __| __|
+    | |  __/\__ \__ \  __/ | | (_| | (__| |_ 
+    \_/\___||___/___/\___|_|  \__,_|\___|\__|                                                                                   
+ 
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+"""
 Class TextFormat():    
     GREEN = '\x1b[1;32;40m'
     RED = '\x1b[1;31;40m'
